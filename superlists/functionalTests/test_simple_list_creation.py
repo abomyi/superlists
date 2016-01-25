4be9f4675f259c6f4f21ef91dbed23a93b1f8208
abomyi <abomyi@abomyi>
2016-01-25 19:09:30 +0800

    完成一個TDD清單簡易實例

--- superlists/functionalTests/test_simple_list_creation.py
+++ superlists/functionalTests/test_simple_list_creation.py
@@ -18,7 +18,7 @@ class NewVisitorTest(FunctionalTest):
 
 
         # 網站邀請她輸入一個待辦事項
-        inputBox = self.browser.find_element_by_id('newItem')
+        inputBox = self.get_item_input_box()
         self.assertEqual(
             inputBox.get_attribute('placeholder'),
             '輸入一個待辦事項'
@@ -35,7 +35,7 @@ class NewVisitorTest(FunctionalTest):
         
         # 頁面另外還有一個文字框，邀請她再加入其他項目，她輸入了「利用孔雀羽毛來做一個路亞」
         # (彤彤做事很講究章法的)
-        inputBox = self.browser.find_element_by_id('newItem')
+        inputBox = self.get_item_input_box()
         inputBox.send_keys('利用孔雀羽毛來做一個路亞')
         inputBox.send_keys(Keys.ENTER)
         
@@ -55,7 +55,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertNotIn('利用孔雀羽毛來做一個路亞', pageText)
         
         # 翔翔建立一個新的清單以及一個新的項目，他不像彤彤一樣那麼有趣
-        inputBox = self.browser.find_element_by_id('newItem')
+        inputBox = self.get_item_input_box()
         inputBox.send_keys('買鮮奶')
         inputBox.send_keys(Keys.ENTER)
 
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,26 +1,38 @@
-from django.shortcuts import render, redirect
+from django.core.exceptions import ValidationError
 from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
+from django.utils.html import escape
+
+from lists.forms import ItemForm
 from lists.models import Item, List
 
 
 def homePage(request):
-    return render(request, 'lists/home.html')
+    return render(request, 'lists/home.html', {'form':ItemForm()})
 
 
 def viewList(request, listID):
     list_ = List.objects.get(id=listID)
-    if request.method=='POST':
-        Item.objects.create(text=request.POST['itemText'], list=list_)
+    if request.method=='GET':
+        return render(request, 'lists/list.html', {'list':list_, 'form':ItemForm()})
+    # POST
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        form.save(forList=list_)
         return redirect(reverse('lists:viewList', args=(list_.id, )))
-    items = Item.objects.filter(list=list_)
-    return render(request, 'lists/list.html', {'list':list_})
+    return render(request, 'lists/list.html', {'list':list_, 'form':form})
 
 
 def newList(request):
-    list_ = List.objects.create()
-    Item.objects.create(text=request.POST.get('itemText'), list=list_)
-    return redirect(reverse('lists:viewList', args=(list_.id, )))
-
+    if request.method=='GET':
+        return render(request, 'lists/home.html', {'form':ItemForm()})
+    # POST    
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        form.save(forList=list_)
+        return redirect(reverse('lists:viewList', args=(list_.id, )))
+    return render(request, 'lists/home.html', {'form':form})
 
 
 
